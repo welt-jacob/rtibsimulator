d3d8949fa22a0bc7fd212a02dd7b72e5719b79af
welt-jacob <157348774+welt-jacob@users.noreply.github.com>
2024-06-05 14:57:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def main():
 
         csv_file = st.file_uploader('CSV 파일 업로드', type=['csv'])
 
-        print(csv_file)
+        #print(csv_file)
         if csv_file is not None:
             current_time = datetime.now()
             filename = current_time.isoformat().replace(':', '_') + '.csv'
