44feae833b13cadede6db79439a9bee7c1f8a71d
welt-jacob <157348774+welt-jacob@users.noreply.github.com>
2024-06-05 11:55:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,26 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-csv_file = st.file_uploader('CSV 파일 업로드', type=['csv'])
+df = st.file_uploader('CSV 파일 업로드', type=['csv'])
+
+df['LOT'] = pd.to_datetime(df['LOT'], format='%Y/%m/%d %H:%M')
+df['AET'] = pd.to_datetime(df['AET'], format='%Y/%m/%d %H:%M')
+
+df['AST'] = df['LOT'] + pd.to_timedelta(df['SOL'], unit='m')
+df['AST'] = pd.to_datetime(df['AST'], format='%Y/%m/%d %H:%M')
+
+df['WASO'] = pd.to_timedelta(df['WASO'], unit='m')
+
+# Calculate the new column
+df['TST'] = (df['AET'] - df['AST'] - pd.to_timedelta(df['WASO'], unit='m'))
+
+df['TST'] = df['TST'].dt.total_seconds() / 60
+df['WASO'] = df['WASO'].dt.total_seconds() / 60
+
+# Calculate the new column 'DSE'
+df['DSE'] = (pd.to_datetime(df['AET']) - pd.to_datetime(df['LOT']))
+
+# Convert the resulting timedelta to minutes
+df['DSE'] = df['DSE'].dt.total_seconds() / 60
+
+df['SE'] = df['TST']/df['DSE']
